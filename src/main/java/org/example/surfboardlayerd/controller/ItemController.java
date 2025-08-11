d288a3a10e2336ca1cbde74098ec3b6723a4d943
Hashini Devindi <hashinidevindi558@gmail.com>
2025-08-11 15:35:40 +0530

    7th

--- src/main/java/org/example/surfboardlayerd/controller/ItemController.java
+++ src/main/java/org/example/surfboardlayerd/controller/ItemController.java
@@ -2,14 +2,19 @@ package org.example.surfboardlayerd.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import org.example.surfboardlayerd.BO.BOFactory;
+import org.example.surfboardlayerd.BO.custom.ItemBO;
+import org.example.surfboardlayerd.model.ItemDto;
+
+import java.sql.SQLException;
+
+
+
 
 public class ItemController {
-
+private  final ItemBO itemBO = (ItemBO) BOFactory.getInstance().getBO(BOFactory.BOTypes.Item);
     @FXML
     private TableColumn<?, ?> colAvailability;
 
@@ -44,12 +49,17 @@ public class ItemController {
     private TextField txtType;
 
     @FXML
-    void btnDeleteOnAction(ActionEvent event) {
-
+    void btnDeleteOnAction(ActionEvent event) throws SQLException, ClassNotFoundException {
+        if (itemBO.delete(lblid.getText())) {
+            loadTable();
+            lblid.setText(itemBO.getNextId());
+            new Alert(Alert.AlertType.INFORMATION, "Deleted!").show();
+        }
     }
 
     @FXML
     void btnGenerateOnAction(ActionEvent event) {
+
 
     }
 
@@ -60,17 +70,44 @@ public class ItemController {
 
     @FXML
     void btnSaveOnAction(ActionEvent event) {
-
+        ItemDto dto = new ItemDto(
+                lblid.getText(),
+                txtName.getText(),
+                Integer.parseInt(txtAvailability.getText()),
+                Double.parseDouble(txtCondition.getText())
+        );
+        if (ItemBO.save(dto)) {
+            loadTable();
+            lblid.setText(ItemBO.getNextId());
+            new Alert(Alert.AlertType.INFORMATION, "Saved!").show();
+        }
     }
 
     @FXML
     void btnUpdateOnAction(ActionEvent event) {
+        ItemDto dto = new ItemDto(
+                lblid.getText(),
+                txtName.getText(),
+                Integer.parseInt(txtAvailability.getText()),
+                Double.parseDouble(txtCondition.getText())
+        );
+        if (ItemBO.update(dto)) {
+            loadTable();
+            lblid.setText(ItemBO.getNextId());
+            new Alert(Alert.AlertType.INFORMATION, "Updated!").show();
+        }
 
     }
 
     @FXML
     void tblClickOnAction(MouseEvent event) {
-
+        ItemDto selected = tblItems.getSelectionModel().getSelectedItem();
+        if (selected != null) {
+            lblid.setText(selected.getItemId());
+            txtName.setText(selected.getName());
+            txtAvailability.setText(String.valueOf(selected.getQtyOnHand()));
+            txtAvailability.setText(String.valueOf(selected.getUnitPrice()));
+        }
     }
 
 }
--- src/main/java/org/example/surfboardlayerd/model/ItemDto.java
+++ src/main/java/org/example/surfboardlayerd/model/ItemDto.java
@@ -14,4 +14,13 @@ public class ItemDto {
     private String conditions;
     private String availabilityStatus;
 
+    public ItemDto(String text, String text1, int i, double v) {
+        this.itemId = text;
+        this.name = text1;
+        this.type = String.valueOf(i);
+        this.conditions = String.valueOf(v);
+    }
+
+    public char[] getQtyOnHand() {
+    }
 }
